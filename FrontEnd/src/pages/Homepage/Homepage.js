d901b35db4da7c19daeb0417d42d67543e672bf7
chauhanhritik <56185071+chauhanhritik@users.noreply.github.com>
2022-01-13 14:20:38 +0530

    ques change

--- FrontEnd/src/pages/Homepage/Homepage.js
+++ FrontEnd/src/pages/Homepage/Homepage.js
@@ -2,8 +2,11 @@ import React from 'react'
 import "./homepage.css"
 import Sidemenu from './Sidemenu'
 import Logoside from './Logoside'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Homepage() {
+    
     return (
         <div className='outermost'>
            <Logoside/>
--- FrontEnd/src/pages/questions/QuestionsLayout.js
+++ FrontEnd/src/pages/questions/QuestionsLayout.js
@@ -1,19 +1,29 @@
 import "./questions.css";
 import React from 'react'
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 export default function Questions(props) {
   return (
     <>
       <div className="container">
         <div className="row">
-          <div className="childsno">{props.sno}</div>
+          <div className="childsno">{props.sno}  
+          </div>
           <div className="childquestion">
             {props.question} 
+             {/* <br /> */}
+          {/* <Switch>
+            <Route ></Route>
+          </Switch> */}
+    {/* half working-->     <Link to={`/$"https://stackoverflow.com/questions/30115324/pass-props-in-link-react-router"?backUrl=$"www.youtube.com"`} >click </Link> */}
+    <a href="$`{props.solution}`" rel="noreferrer">
+    FAQ
+  </a>
           </div>
            
           <div className="childstatus">
             {props.mode}
           </div>
+
         </div>
       </div>
     </>
